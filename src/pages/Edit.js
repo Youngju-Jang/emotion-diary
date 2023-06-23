8ae4f2daa5e28333ca2da486e7378b10fd8923a6
Youngju-Jang <joj1043@nate.com>
2023-06-23 11:52:45 +0900

    [FEAT] useParams, useSearchParams, useNavigate
    related to #1

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,19 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  console.log(id);
+
+  const mode = searchParams.get("mode");
+  console.log(mode);
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 Edit입니다.</p>
+      <button onClick={() => setSearchParams({ who: "winterlood" })}>QS 바꾸기</button>
+      <button onClick={() => navigate(-1)}></button>
     </div>
   );
 };
