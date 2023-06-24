2ee04ea7de06eb66b2d3fc6e7f5e3cdcb6207eee
Youngju-Jang <joj1043@nate.com>
2023-06-24 21:13:12 +0900

    [Feat] 상세페이지 - 중간섹션 감정부분 구현 & 중복유틸 분리(emotionList)
    related to #4

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import { DiaryStateContext } from "../App";
 
 import { getStringDate } from "../util/date";
+import { emotionList } from "../util/Emotion";
+
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 
@@ -30,6 +32,10 @@ const Diary = () => {
   if (!data) {
     return <div className="DiaryPage">로딩중입니다...</div>;
   } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+    console.log(curEmotionData);
     return (
       <div className="DiaryPage">
         <MyHeader
@@ -37,6 +43,15 @@ const Diary = () => {
           headText={`${getStringDate(new Date(data.date))} 기록`}
           rightChild={<MyButton text={"수정하기"} onClick={() => navigate(`/edit/${data.id}`)} />}
         />
+        <article>
+          <section>
+            <h4>오늘의 감정</h4>
+            <div className={["diary_img_wrapper", `diary_img_wrapper_${data.emotion}`].join(" ")}>
+              <img src={curEmotionData.emotion_img} />
+              <div className="emotion_descript">{curEmotionData.emotion_descript}</div>
+            </div>
+          </section>
+        </article>
       </div>
     );
   }
